67e0b7d83c71c5607b1c4107bee1c08451200527
Rafael García <rafael.garcia@scopely.com>
2023-02-10 20:15:57 +0000

    Story reads the version from json

---
+++ src/story.rs
@@ -0,0 +1,72 @@
+#![allow(unused_variables, dead_code)]
+
+use crate::json_serialization;
+
+const INK_VERSION_CURRENT: i32 = 21;
+const INK_VERSION_MINIMUM_COMPATIBLE: i32 = 18;
+
+pub struct Story {}
+
+impl Story {
+    pub fn new(json_string: &str) -> Result<Self, String> {
+        let json: serde_json::Value = match serde_json::from_str(json_string) {
+            Ok(value) => value,
+            Err(_) => return Err("Story not in JSON format.".to_string()),
+        };
+
+        let version_opt = json.get("inkVersion");
+
+        if version_opt.is_none() || !version_opt.unwrap().is_number() {
+            return Err(
+                "ink version number not found. Are you sure it's a valid .ink.json file?"
+                    .to_string(),
+            );
+        }
+
+        let version: i32 = version_opt.unwrap().as_i64().unwrap().try_into().unwrap();
+
+        if version > INK_VERSION_CURRENT {
+            return Err("Version of ink used to build story was newer than the current version of the engine".to_string());
+        } else if version < INK_VERSION_MINIMUM_COMPATIBLE {
+            return Err("Version of ink used to build story is too old to be loaded by this version of the engine".to_string());
+        } else if version != INK_VERSION_CURRENT {
+            log::debug!("WARNING: Version of ink used to build story doesn't match current version of engine. Non-critical, but recommend synchronising.");
+        }
+
+        let rootToken = match json.get("root") {
+            Some(value) => value,
+            None => {
+                return Err(
+                    "Root node for ink not found. Are you sure it's a valid .ink.json file?"
+                        .to_string(),
+                )
+            }
+        };
+
+        //object listDefsObj;
+        //if (rootObject.TryGetValue ("listDefs", out listDefsObj)) {
+        //    _listDefinitions = Json.JTokenToListDefinitions (listDefsObj);
+        //}
+
+        //_mainContentContainer = Json.JTokenToRuntimeObject (rootToken) as Container;
+        json_serialization::jtoken_to_runtime_object(rootToken);
+
+        //ResetState ();
+
+        Ok(Story {})
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use std::fs;
+
+    use super::*;
+
+    #[test]
+    fn create_test() {
+        let json_string =
+            fs::read_to_string("examples/inkfiles/basictext/oneline.ink.json").unwrap();
+        Story::new(&json_string).unwrap();
+    }
+}
