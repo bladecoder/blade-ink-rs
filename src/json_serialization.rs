2edb2bcd5b7b81f87ae1eb297009acde8de4f951
Rafael García <bladecoder@gmail.com>
2023-02-10 20:15:57 +0000

    Story reads the version from json

---
+++ src/json_serialization.rs
@@ -0,0 +1,15 @@
+use serde_json::Value;
+
+pub fn jtoken_to_runtime_object(token: &Value) {
+
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn basic_test() {
+        
+    }
+}
