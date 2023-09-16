14f53a5be28e9b18d2075e5eeec37ed1bb3deb7c
Rafael García <bladecoder@gmail.com>
2023-09-16 22:29:07 +0000

    Oneline test passed.

--- src/choice.rs
+++ src/choice.rs
@@ -1,29 +1,43 @@
-use std::rc::Rc;
+use core::fmt;
 
-use crate::{path::Path, callstack::Thread};
+use crate::{path::Path, callstack::Thread, object::{Object, RTObject}};
 
-pub struct Choice {
-    target_path: Path,
-    is_invisible_default: bool,
-    tags: Vec<String>,
-    index: usize,
-    original_thread_index: usize,
-    text: String,
-    thread_at_generation: Rc<Thread>,
-    source_path: String
+pub(crate) struct Choice {
+    obj: Object,
+    pub target_path: Path,
+    pub is_invisible_default: bool,
+    pub tags: Vec<String>,
+    pub index: usize,
+    pub original_thread_index: usize,
+    pub text: String,
+    pub thread_at_generation: Thread,
+    pub source_path: String
 }
 
 impl Choice {
-    pub fn new() -> Choice {
+    pub fn new(target_path: Path, source_path: String, is_invisible_default: bool, tags: Vec<String>, thread_at_generation: Thread, text: String, index: usize, original_thread_index: usize) -> Choice {
         Choice {
-            target_path: todo!(),
-            is_invisible_default: todo!(),
-            tags: todo!(),
-            index: todo!(),
-            original_thread_index: todo!(),
-            text: todo!(),
-            thread_at_generation: todo!(),
-            source_path: todo!(),
+            obj: Object::new(),
+            target_path: target_path,
+            is_invisible_default: is_invisible_default,
+            tags: tags,
+            index: index,
+            original_thread_index: original_thread_index,
+            text: text,
+            thread_at_generation: thread_at_generation,
+            source_path: source_path,
         }
+    }
+}
+
+impl RTObject for Choice {
+    fn get_object(&self) -> &Object {
+        &self.obj
+    }
+}
+
+impl fmt::Display for Choice {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        write!(f, "**Choice**")
     }
 }
--- src/flow.rs
+++ src/flow.rs
@@ -1,19 +1,19 @@
 use std::{rc::Rc, cell::RefCell};
 
-use crate::{callstack::CallStack, story::Story, choice::Choice, object::RTObject};
+use crate::{callstack::CallStack, choice::Choice, object::RTObject, container::Container};
 
 pub(crate) struct Flow {
     pub name: String,
     pub callstack: Rc<RefCell<CallStack>>,
     pub output_stream: Vec<Rc<dyn RTObject>>,
-    pub current_choices: Vec<Choice>
+    pub current_choices: Vec<Rc<Choice>>
 }
 
 impl Flow {
-    pub fn new(name: &str, story: &Story) -> Flow {
+    pub fn new(name: &str, main_content_container: Rc<Container>) -> Flow {
         Flow { 
             name: name.to_string(),
-            callstack: Rc::new(RefCell::new(CallStack::new(story))),
+            callstack: Rc::new(RefCell::new(CallStack::new(main_content_container))),
             output_stream: Vec::new(),
             current_choices: Vec::new()
         }
