06297609ba2c7df2bb18545fc6cba50d82cf50d1
Rafael García <rafael.garcia@scopely.com>
2023-09-24 21:08:34 +0000

    all knot tests passes.

--- tests/knot_test.rs
+++ tests/knot_test.rs
@@ -3,9 +3,77 @@ use bladeink::story::Story;
 mod common;
 
 #[test]
-fn gather_basic_test() -> Result<(), String>  {
+fn single_line_test() -> Result<(), String>  {
     let json_string =
-        common::get_json_string("examples/inkfiles/gather/gather-basic.ink.json").unwrap();
+        common::get_json_string("examples/inkfiles/knot/single-line.ink.json").unwrap();
+    let mut story = Story::new(&json_string).unwrap();
+    let mut text: Vec<String> = Vec::new();
+
+    common::next_all(&mut story, &mut text)?;
+
+    assert_eq!(1, text.len());
+    assert_eq!("Hello, world!", text[0]);
+
+    Ok(())
+}
+
+#[test]
+fn multi_line_test() -> Result<(), String>  {
+    let json_string =
+        common::get_json_string("examples/inkfiles/knot/multi-line.ink.json").unwrap();
+    let mut story = Story::new(&json_string).unwrap();
+    let mut text: Vec<String> = Vec::new();
+
+    common::next_all(&mut story, &mut text)?;
+
+    assert_eq!(3, text.len());
+    assert_eq!("Hello, world!", text[0]);
+    assert_eq!("Hello?", text[1]);
+    assert_eq!("Hello, are you there?", text[2]);
+
+    Ok(())
+}
+
+#[test]
+fn strip_empty_lines_test() -> Result<(), String>  {
+    let json_string =
+        common::get_json_string("examples/inkfiles/knot/strip-empty-lines.ink.json").unwrap();
+    let mut story = Story::new(&json_string).unwrap();
+    let mut text: Vec<String> = Vec::new();
+
+    common::next_all(&mut story, &mut text)?;
+
+    assert_eq!(3, text.len());
+    assert_eq!("Hello, world!", text[0]);
+    assert_eq!("Hello?", text[1]);
+    assert_eq!("Hello, are you there?", text[2]);
+
+    Ok(())
+}
+
+#[test]
+fn param_strings_test() -> Result<(), String>  {
+    let json_string =
+        common::get_json_string("examples/inkfiles/knot/param-strings.ink.json").unwrap();
+    let mut story = Story::new(&json_string).unwrap();
+    let mut text: Vec<String> = Vec::new();
+
+    common::next_all(&mut story, &mut text)?;
+    story.choose_choice_index(2);
+
+    text.clear();
+    common::next_all(&mut story, &mut text)?;
+
+    assert_eq!(1, text.len());
+    assert_eq!("\"I accuse myself!\" Poirot declared.", text[0]);
+
+    Ok(())
+}
+
+#[test]
+fn param_ints_test() -> Result<(), String>  {
+    let json_string =
+        common::get_json_string("examples/inkfiles/knot/param-ints.ink.json").unwrap();
     let mut story = Story::new(&json_string).unwrap();
     let mut text: Vec<String> = Vec::new();
 
@@ -15,10 +83,80 @@ fn gather_basic_test() -> Result<(), String>  {
     text.clear();
     common::next_all(&mut story, &mut text)?;
 
-    assert_eq!(3, text.len());
-    assert_eq!("\"Nothing, Monsieur!\" I replied.", text[0]);
-    assert_eq!("\"Very good, then.\"", text[1]);
-    assert_eq!("With that Monsieur Fogg left the room.", text[2]);
+    assert_eq!(1, text.len());
+    assert_eq!("You give 2 dollars.", text[0]);
+
+    Ok(())
+}
+
+#[test]
+fn param_floats_test() -> Result<(), String>  {
+    let json_string =
+        common::get_json_string("examples/inkfiles/knot/param-floats.ink.json").unwrap();
+    let mut story = Story::new(&json_string).unwrap();
+    let mut text: Vec<String> = Vec::new();
+
+    common::next_all(&mut story, &mut text)?;
+    story.choose_choice_index(1);
+
+    text.clear();
+    common::next_all(&mut story, &mut text)?;
+
+    assert_eq!(1, text.len());
+    assert_eq!("You give 2.5 dollars.", text[0]);
+
+    Ok(())
+}
+
+#[test]
+fn param_vars_test() -> Result<(), String>  {
+    let json_string =
+        common::get_json_string("examples/inkfiles/knot/param-vars.ink.json").unwrap();
+    let mut story = Story::new(&json_string).unwrap();
+    let mut text: Vec<String> = Vec::new();
+
+    common::next_all(&mut story, &mut text)?;
+    story.choose_choice_index(1);
+
+    text.clear();
+    common::next_all(&mut story, &mut text)?;
+
+    assert_eq!(1, text.len());
+    assert_eq!("You give 2 dollars.", text[0]);
+
+    Ok(())
+}
+
+#[test]
+fn param_multi_test() -> Result<(), String>  {
+    let json_string =
+        common::get_json_string("examples/inkfiles/knot/param-multi.ink.json").unwrap();
+    let mut story = Story::new(&json_string).unwrap();
+    let mut text: Vec<String> = Vec::new();
+
+    common::next_all(&mut story, &mut text)?;
+    story.choose_choice_index(0);
+
+    text.clear();
+    common::next_all(&mut story, &mut text)?;
+
+    assert_eq!(1, text.len());
+    assert_eq!("You give 1 or 2 dollars. Hmm.", text[0]);
+
+    Ok(())
+}
+
+#[test]
+fn param_recurse_test() -> Result<(), String>  {
+    let json_string =
+        common::get_json_string("examples/inkfiles/knot/param-recurse.ink.json").unwrap();
+    let mut story = Story::new(&json_string).unwrap();
+    let mut text: Vec<String> = Vec::new();
+
+    common::next_all(&mut story, &mut text)?;
+
+    assert_eq!(2, text.len());
+    assert_eq!("\"The result is 120!\" you announce.", text[0]);
 
     Ok(())
 }
