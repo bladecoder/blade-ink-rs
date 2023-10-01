a592d2f65290099e52d73cffdf2450f0574c2512
Rafael García <bladecoder@gmail.com>
2023-10-01 18:40:40 +0000

    Multiflow tests passed

--- tests/multi_flow_test.rs
+++ tests/multi_flow_test.rs
@@ -53,7 +53,35 @@ fn multiflow_save_load_threads() -> Result<(), String>  {
     assert_eq!("Thread 1 red choice", story.get_current_choices()[0].text);
 
     // Save/load test
-    // let saved = story.getState().toJson();
+    let saved = story.get_state().to_json();
+
+    // Test choice before reloading state before resetting
+    story.choose_choice_index(0);
+    assert_eq!("Thread 1 red choice\nAfter thread 1 choice (red)\n", story.continue_maximally()?);
+    let mut story = Story::new(&json_string).unwrap();
+
+    // Load to pre-choice: still red, choose second choice
+    story.get_state_mut().load_json(&saved)?;
+
+    story.choose_choice_index(1);
+    assert_eq!("Thread 2 red choice\nAfter thread 2 choice (red)\n", story.continue_maximally()?);
+
+    // Load: switch to blue, choose 1
+    story.get_state_mut().load_json(&saved)?;
+    story.switch_flow("Blue Flow");
+    story.choose_choice_index(0);
+    assert_eq!("Thread 1 blue choice\nAfter thread 1 choice (blue)\n", story.continue_maximally()?);
+
+    // Load: switch to blue, choose 2
+    story.get_state_mut().load_json(&saved)?;
+    story.switch_flow("Blue Flow");
+    story.choose_choice_index(1);
+    assert_eq!("Thread 2 blue choice\nAfter thread 2 choice (blue)\n", story.continue_maximally()?);
+
+    // Remove active blue flow, should revert back to global flow
+    story.remove_flow("Blue Flow");
+    assert_eq!("Default line 2\n", story.cont()?);
+
 
     Ok(())
 }
