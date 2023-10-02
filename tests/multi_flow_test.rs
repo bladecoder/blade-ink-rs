18e83879237cd81528ffcbfcc1115e01be5e3954
Rafael García <rafael.garcia@scopely.com>
2023-10-02 17:25:55 +0000

    Story.state as not optional

--- tests/multi_flow_test.rs
+++ tests/multi_flow_test.rs
@@ -8,18 +8,18 @@ fn basics_test() -> Result<(), StoryError>  {
         common::get_json_string("examples/inkfiles/runtime/multiflow-basics.ink.json").unwrap();
     let mut story = Story::new(&json_string).unwrap();
 
-    story.switch_flow("First");
+    story.switch_flow("First")?;
     story.choose_path_string("knot1", true, None)?;
     assert_eq!("knot 1 line 1\n", story.cont()?);
 
-    story.switch_flow("Second");
+    story.switch_flow("Second")?;
     story.choose_path_string("knot2", true, None)?;
     assert_eq!("knot 2 line 1\n", story.cont()?);
 
-    story.switch_flow("First");
+    story.switch_flow("First")?;
     assert_eq!("knot 1 line 2\n", story.cont()?);
 
-    story.switch_flow("Second");
+    story.switch_flow("Second")?;
     assert_eq!("knot 2 line 2\n", story.cont()?);
 
     Ok(())
@@ -34,22 +34,22 @@ fn multiflow_save_load_threads() -> Result<(), StoryError>  {
     // Default flow
     assert_eq!("Default line 1\n", story.cont()?);
 
-    story.switch_flow("Blue Flow");
+    story.switch_flow("Blue Flow")?;
     story.choose_path_string("blue", true, None)?;
     assert_eq!("Hello I'm blue\n", story.cont()?);
 
-    story.switch_flow("Red Flow");
+    story.switch_flow("Red Flow")?;
     story.choose_path_string("red", true, None)?;
     assert_eq!("Hello I'm red\n", story.cont()?);
 
     // Test existing state remains after switch (blue)
-    story.switch_flow("Blue Flow");
-    assert_eq!("Hello I'm blue\n", story.get_current_text());
+    story.switch_flow("Blue Flow")?;
+    assert_eq!("Hello I'm blue\n", story.get_current_text()?);
     assert_eq!("Thread 1 blue choice", story.get_current_choices()[0].text);
 
     // Test existing state remains after switch (red)
-    story.switch_flow("Red Flow");
-    assert_eq!("Hello I'm red\n", story.get_current_text());
+    story.switch_flow("Red Flow")?;
+    assert_eq!("Hello I'm red\n", story.get_current_text()?);
     assert_eq!("Thread 1 red choice", story.get_current_choices()[0].text);
 
     // Save/load test
@@ -68,13 +68,13 @@ fn multiflow_save_load_threads() -> Result<(), StoryError>  {
 
     // Load: switch to blue, choose 1
     story.get_state_mut().load_json(&saved)?;
-    story.switch_flow("Blue Flow");
+    story.switch_flow("Blue Flow")?;
     story.choose_choice_index(0);
     assert_eq!("Thread 1 blue choice\nAfter thread 1 choice (blue)\n", story.continue_maximally()?);
 
     // Load: switch to blue, choose 2
     story.get_state_mut().load_json(&saved)?;
-    story.switch_flow("Blue Flow");
+    story.switch_flow("Blue Flow")?;
     story.choose_choice_index(1);
     assert_eq!("Thread 2 blue choice\nAfter thread 2 choice (blue)\n", story.continue_maximally()?);
 
